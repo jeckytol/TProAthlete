d75794eff62796f428884256dbeb51d7ae8b15eb
jeckytol <jeckyk1ufc@gmail.com>
2025-04-29 00:29:08 +0300

    Initial Commit

---
+++ PunchKickTraining/PunchKickTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunchKickTrainingApp.swift
+//  PunchKickTraining
+//
+//  Created by Jecky Toledo on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PunchKickTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PunchKickTrainingTests/PunchKickTrainingTests.swift
@@ -0,0 +1,16 @@
+//
+//  PunchKickTrainingTests.swift
+//  PunchKickTrainingTests
+//
+//  Created by Jecky Toledo on 29/04/2025.
+//
+
+import Testing
+
+struct PunchKickTrainingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
